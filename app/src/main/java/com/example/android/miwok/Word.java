5c72d2e73ec0f3c43329ef65a111d50ecc5e00e7
Gitling01 <Faylor52@gmail.com>
2020-12-02 18:42:54 -0500

    Commit of the final project, now with tabs

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -2,30 +2,33 @@ package com.example.android.miwok;
 
 public class Word {
 
+    private static final int NO_IMAGE_PROVIDED = -1;
     private String mMiwokTranslation;
     private String mEnglishTranslation;
-    private static final int NO_IMAGE_PROVIDED = -1;
     private int mImageResourceId;
+    private int mSoundResourceId;
 
-   //constructor (creating a word object with these two translations)
-   public Word(String miwokTranslation, String defaultTranslation) {
-       mMiwokTranslation = miwokTranslation;
-       mEnglishTranslation = defaultTranslation;
-       mImageResourceId = NO_IMAGE_PROVIDED;
-   }
+    //constructor (creating a word object with these two translations)
+    public Word(String miwokTranslation, String defaultTranslation, int SoundResourceId) {
+        mMiwokTranslation = miwokTranslation;
+        mEnglishTranslation = defaultTranslation;
+        mImageResourceId = NO_IMAGE_PROVIDED;
+        mSoundResourceId = SoundResourceId;
+    }
 
-    public Word(String miwokTranslation, String defaultTranslation, int ImageResourceId) {
+    public Word(String miwokTranslation, String defaultTranslation, int ImageResourceId, int SoundResourceId) {
         mMiwokTranslation = miwokTranslation;
         mEnglishTranslation = defaultTranslation;
         mImageResourceId = ImageResourceId;
+        mSoundResourceId = SoundResourceId;
     }
 
     //retrieving the translations
-    public String getMiwokTranslation(){
+    public String getMiwokTranslation() {
         return mMiwokTranslation;
     }
 
-    public String getDefaultTranslation(){
+    public String getDefaultTranslation() {
         return mEnglishTranslation;
     }
 
@@ -33,6 +36,19 @@ public class Word {
         return mImageResourceId;
     }
 
+    public int getSoundResourceId() {
+        return mSoundResourceId;
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "mMiwokTranslation='" + mMiwokTranslation + '\'' +
+                ", mEnglishTranslation='" + mEnglishTranslation + '\'' +
+                ", mImageResourceId=" + mImageResourceId +
+                ", mSoundResourceId=" + mSoundResourceId +
+                '}';
+    }
 
     /**
      * Returns whether or not there is an image for this word
